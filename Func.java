b1264a24f9d3c10d1963bec0a538596610f9368f
Mika <61342907+Mikhail666999@users.noreply.github.com>
2022-02-19 02:54:20 +0500

    Add files via upload

--- Func.java
+++ Func.java
@@ -1,20 +1,21 @@
 public class Func{
-	public static void println(Object x){
+	public static void print(Object x){
 		System.out.println(x);
 	}
 
-	public static void print(Object x){
-		System.out.print(x);
-	}
 
-	public static void find(String text, String word_which_nado_compute){
+	public static void find(String text, String[] word_which_nado_compute, int word_which_nado_compute_length){
 		int x = 0;
+		int i = 0;
 		String[] words = text.split(" ");
-		for(String word : words){
-			if(word.contains(word_which_nado_compute)){
-				x++;
+		while (i != word_which_nado_compute_length ) {
+			for(String word : words){
+				if(word.contains(word_which_nado_compute[i])){
+					x++;
+				}
 			}
+			i++;
 		}
-		println(x);
+		print(x);
 	}
 }
--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ class Main{
 		Func f = new Func();
 
 		String text = "орк зад макака мозги резня орк мозги феменистка мозги орк тупой олег олег трутень орк мозги лопата макс феменистка феменистка мозги мозги мозги мозги зад задорк мозги феменистка мозги орк тупой олег олег трутень орк мозги лопата макс  зад орк орк орк мозги феменистка мозги орк тупой олег олег трутень орк мозги лопата макс  орк орк орк ";
-		String word_which_nado_compute = "орк";
-		f.find(text, word_which_nado_compute);
+		String[] word_which_nado_compute = new String[]	{"орк", "феменистка", "мозги", "олег"};
+		int word_which_nado_compute_length = word_which_nado_compute.length;
+		f.find(text, word_which_nado_compute, word_which_nado_compute_length);
+		f.print("Кол-во слов коорые надо посчитать: " + word_which_nado_compute_length);
 	}	
 }
